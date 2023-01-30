5576575d653abd357d69f2c43de3e3f9d894c1de
Boris Povolotsky <boristofu@gmail.com>
2023-01-30 21:46:50 +0200

    fixed some configurations, added a new article about pro m54 review

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import netlify from "@astrojs/netlify/functions";
-import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), solid(), mdx(), sitemap()],
-  site: `https://www.drippingcoffee.com`,
-  adapter: netlify()
+  integrations: [mdx(), sitemap()],
+  site: 'https://www.drippingcoffee.com/',
 });
