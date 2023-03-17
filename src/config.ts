b50942ad5f45b91fbe353186beaf05b27de69c0b
Boris Povolotsky <boristofu@gmail.com>
2023-03-17 17:05:37 +0200

    try to accomodate styles for search console warning that the site is not mobile friendly

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@
 export const SITE_TITLE = 'Dripping Coffee';
 export const SITE_DESCRIPTION = 'Dripping Coffee Reviews All Things Home Coffee Brewing';
 
-export const HERO_TITLE = `Where Coffee Drips...`
+export const HERO_TITLE = `Your Coffee`
 export const HERO_SUBTITLE = 'We got you covered with everything from A-Z: Gear, techniques, discussions and whatnot'
 
 export const HERO_BG_IMG = 'https://craftcoffeespot.com/wp-content/uploads/2021/11/CCS-Home-Page-Background-Compressed.png'
