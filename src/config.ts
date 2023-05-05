be15ffe3895ee8a75f7ec2eb34a9d5f48a0b0535
Boris Povolotsky <boristofu@gmail.com>
2023-05-06 00:12:39 +0300

    restyle hero preview posts, fix couple of internal links, fixed header nav

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const SITE_DESCRIPTION = 'Dripping Coffee Reviews All Things Home Coffee 
 
 export const HERO_TITLE = `Coffee With A Twist`
 // export const HERO_SUBTITLE = 'We got you covered with everything from A-Z: Gear, techniques, discussions and whatnot'
-export const HERO_SUBTITLE = "I share everything I know and learn about coffee. Caffeine, Espresso, Techniques, Recipes, And Whatnot?!"
+export const HERO_SUBTITLE = "I share everything I know and learn about all things Coffee, Caffeine, Espresso, Techniques & Recipes"
 
 // export const HERO_BG_IMG = 'https://craftcoffeespot.com/wp-content/uploads/2021/11/CCS-Home-Page-Background-Compressed.png'
 export const HERO_BG_IMG = '/assets/myself.jpg'
