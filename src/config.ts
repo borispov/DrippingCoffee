39ae59346a815489d3f4bad347e7f3acaad81d37
Boris Povolotsky <boristofu@gmail.com>
2023-05-05 17:07:52 +0300

    new article and some design elements

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,9 @@
 export const SITE_TITLE = 'Dripping Coffee';
 export const SITE_DESCRIPTION = 'Dripping Coffee Reviews All Things Home Coffee Brewing';
 
-export const HERO_TITLE = `Your Coffee`
-export const HERO_SUBTITLE = 'We got you covered with everything from A-Z: Gear, techniques, discussions and whatnot'
+export const HERO_TITLE = `Coffee With A Twist`
+// export const HERO_SUBTITLE = 'We got you covered with everything from A-Z: Gear, techniques, discussions and whatnot'
+export const HERO_SUBTITLE = "I share everything I know and learn about coffee. Caffeine, Espresso, Techniques, Recipes, And Whatnot?!"
 
-export const HERO_BG_IMG = 'https://craftcoffeespot.com/wp-content/uploads/2021/11/CCS-Home-Page-Background-Compressed.png'
+// export const HERO_BG_IMG = 'https://craftcoffeespot.com/wp-content/uploads/2021/11/CCS-Home-Page-Background-Compressed.png'
+export const HERO_BG_IMG = '/assets/myself.jpg'
