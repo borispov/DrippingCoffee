233ffd3b37d3aa6891f2149246beb7aea7131a60
Boris Povolotsky <boristofu@gmail.com>
2023-08-23 01:21:53 +0300

    edit articles and hopefully finish the redirect drama

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true
-  },
+  trailingSlash: 'always',
   integrations: [mdx(), sitemap(), solidJs(), svelte()],
   site: 'https://www.drippingcoffee.com/',
 });
