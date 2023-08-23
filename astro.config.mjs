92e1f50030a51d0eb0b0559c730cbb3c95cd61fa
Boris Povolotsky <boristofu@gmail.com>
2023-08-23 23:49:58 +0300

    change www to no www

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ import svelte from "@astrojs/svelte";
 export default defineConfig({
   trailingSlash: 'always',
   integrations: [mdx(), sitemap(), solidJs(), svelte()],
-  site: 'https://www.drippingcoffee.com/',
+  site: 'https://drippingcoffee.com/',
 });
