5c9cfabc213dad96a54f0a4ada32adc6a20b5678
Boris Povolotsky <boristofu@gmail.com>
2023-08-27 02:16:39 +0300

    added ToC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,36 @@
+// import { loadEnv } from "vite";
+// const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+// if (
+//   !PUBLIC_SANITY_PROJECT_ID || !PUBLIC_SANITY_DATASET
+// ) {
+//   throw new Error("Did you forget to run sanity init --env?");
+// }
+
 import { defineConfig } from 'astro/config';
+import remarkToc from 'remark-toc';
+// import sanity from "@sanity/astro";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import solidJs from "@astrojs/solid-js";
-
 import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   trailingSlash: 'always',
-  integrations: [mdx(), sitemap(), solidJs(), svelte()],
+  markdown: {
+    remarkPlugins: [ [remarkToc, { heading: 'content' }]],
+    drafts: true,
+  },
+  integrations: [
+    // sanity({
+    //   projectId: PUBLIC_SANITY_PROJECT_ID,
+    //   dataset: PUBLIC_SANITY_DATASET,
+    //   useCdn: false,
+    //   apiVersion: "2023-03-20",
+    // }),
+    mdx({ drafts: true }), 
+    sitemap(), 
+    svelte(),
+    react()
+  ],
   site: 'https://drippingcoffee.com/',
 });
